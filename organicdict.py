ee52b413918b2b01138ee1dfa244be1150c1f981
Luiz Fernando <luizf.prodigio@gmail.com>
2019-10-26 23:29:23 -0300

    extract function created

--- organicdict.py
+++ organicdict.py
@@ -10,40 +10,6 @@ class OrganicDict(dict):
         super().__init__(seed)
 
 
-    def mutate(self, item):
-        """
-        item: `dict`, `sequence`
-        
-        Update a path or group of paths.
-        If item is a `dict` instance, merge the calling object with item,
-        comparing all keys in same depth.
-        If item is a sequence, consider thats represent the path to be updated.
-        In any case, conflicting keys will be updated, receiving item value, 
-        and item keys missing on obejct will be created"""
-        
-        assert isinstance(item, (dict, tuple, list)), "item needs to be `dict`, `list` or `tuple` instance"
-        if not isinstance(item, dict):
-            item = self._tuple_to_dict(item)
-        self._merge(self, item)
-
-
-    def search(self, path):
-        """Check for a path in object. If is a valid path, return the value.
-        Otherwise, return `None`"""
-        
-        p = self
-        for k in path:
-            if k not in p: return None
-            p = p[k]
-        return p
-
-
-    def defoliate(self):
-        """Return a generator thats iterate over all values"""
-        
-        return self._get_leafs(self)
-
-
     @classmethod
     def _get_leafs(cls, d):
         for k in d:
@@ -67,3 +33,46 @@ class OrganicDict(dict):
     def _tuple_to_dict(cls, t):
         if not t: return None
         return t[0] if len(t) < 2 else {t[0]: cls._tuple_to_dict(t[1:])}
+        
+        
+    def mutate(self, item):
+        """
+        item: `dict`, `sequence`
+        
+        Update a path or group of paths.
+        If item is a `dict` instance, merge the calling object with item,
+        comparing all keys in same depth.
+        If item is a sequence, consider thats represent the path to be updated.
+        In any case, conflicting keys will be updated, receiving item value, 
+        and item keys missing on obejct will be created"""
+        
+        assert isinstance(item, (dict, tuple, list)), "item needs to be `dict`, `list` or `tuple` instance"
+        if not isinstance(item, dict):
+            item = self._tuple_to_dict(item)
+        self._merge(self, item)
+
+
+    def search(self, path):
+        """Check for a path in object. If is a existent path, return the value.
+        Otherwise, return `None`"""
+        
+        p = self
+        for k in path:
+            if k not in p: return None
+            p = p[k]
+        return p
+
+
+    def defoliate(self):
+        """Return a generator thats iterate over all values"""
+        
+        return self._get_leafs(self)
+
+
+    def extract(self, path):
+        """Return a `OrganicDict` instance with the result of path as
+        content."""
+        member = self.search(path)
+        assert isinstance(member, dict), "Path not exists"
+        return OrganicDict(member)
+        
